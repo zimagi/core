9004a323f4f0f64fcd0a4bf9045d7f6108af623b
Adrian Webb <adrian@webb.sh>
2025-07-07 00:24:19 -0400

    Removing the clear chat option from the chat command mixin and the chat send command.

--- app/commands/chat/send.py
+++ app/commands/chat/send.py
@@ -4,7 +4,12 @@ from systems.commands.index import Command
 class Send(Command("chat.send")):
 
     def exec(self):
-        if self.chat_clear:
-            chat = self._chat.retrieve(f"{self.active_user.name}:{self.chat_name}")
-            chat.clear_messages()
-        self.send("chat:request", {"user": self.active_user.name, "name": self.chat_name, "message": self.chat_message})
+        if self.chat_message:
+            self.send(
+                "chat:request",
+                {
+                    "user": self.active_user.name,
+                    "name": self.chat_name,
+                    "message": self.chat_message,
+                },
+            )
