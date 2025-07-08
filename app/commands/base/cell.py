8f44513b554a99d775390d4c5497d7b126ddec57
Adrian Webb <adrian@webb.sh>
2025-07-08 11:09:49 -0400

    Locking in updats to the Cell agent implementation.

--- app/commands/base/cell.py
+++ app/commands/base/cell.py
@@ -3,7 +3,9 @@ import copy
 import logging
 
 from systems.commands.index import BaseCommand
-from utility.data import normalize_value, load_json
+from utility.data import normalize_value, load_json, flatten_dict
+from utility.validation import validate_flattened_dict
+from utility.display import format_traceback, format_exception_info
 
 
 logger = logging.getLogger(__name__)
@@ -15,59 +17,99 @@ class CellError(Exception):
 
 class Cell(BaseCommand("cell")):
 
+    def __init__(self, name, parent=None):
+        super().__init__(name, parent)
+        if self.agent_user:
+            self._user.set_active_user(self.get_instance(self._user, self.agent_user, required=True))
+
+        self.channels = self.get_channels()
+
+        self._set_sensor(self.agent_sensor)
+        logger.debug(f"Set sensor to: {self.sensor_name}")
+
+    def get_state_key(self):
+        return f"cell:state:{self.agent_user}:{".".join(self.get_full_name().split(" ")[1:])}"
+
+    def get_sensor_key(self):
+        return self.agent_user
+
     def exec(self):
-        state_key = f"cell:state:{".".join(self.get_full_name().split(" "))}"
+        #
+        # Listen (sensor) - listen
+        # Translate (message) - load_message
+        # Perform Action (llm / agent) - perform_action
+        # Translate (message) - format_message
+        # Transmit (transmitter) - send
+        # Evaluate Outcome (llm / agent) - refine_state
+        #
+        state_key = self.get_state_key()
+        sensor_key = self.get_sensor_key()
         logger.info(f"Cell starting with state key: {state_key}")
 
-        self.manager.load_templates()
-        logger.debug(f"Starting to listen for {self.sensor} with key: {self.get_sensor_key()}")
-
-        for package in self.listen(self.sensor, state_key=self.get_sensor_key()):
-            logger.info(f"Message sender: {package.sender}")
-            logger.debug(f"Message received: {package.message}")
-
-            message = self.load_message(package.message)
-            logger.info(f"Message loaded: {message}")
-
-            if self._check_message(message):
-                logger.debug("Message check successful")
-                state = self.get_state(state_key, {"goal": self.goal, "rules": self.rules, "tools": self.tools})
-                logger.debug(f"Loading agent state: {state}")
-
-                self.perform_action(package, message, state)
-                self.set_state(state_key, self.refine_state(package, message, state))
-                logger.debug("Cell cycle complete")
-
-    def perform_action(self, package, message, state):
         try:
-            self.data("Received message from", package.sender)
-            response = self.profile(self._process_message, message, state)
+            state = self.get_state(
+                state_key,
+                {"goal": self.agent_goal, "rules": self.agent_rules, "tools": self.agent_tools},
+            )
+            logger.debug(f"Loaded agent state: {state}")
+
+            logger.debug(f"Starting to listen for {self.sensor_name} with key: {sensor_key}")
+            for package in self.listen(self.sensor_name, state_key=sensor_key):
+                logger.info(f"Message sender: {package.sender}")
+                logger.debug(f"Message received: {package.message}")
+
+                message = self._load_message(package)
+                if message:
+                    logger.info(f"Message loaded: {message}")
+                    try:
+                        self.data("Received message from", package.sender)
+                        response = self.profile(self.process_sensory_message, message, state)
+
+                    except Exception as error:
+                        logger.debug(f"Got an error performing action: {error}")
+                        self.handle_error(error, package)
+                        raise error
+
+                    channel = package.sender if self.channel == "sender" else self.channel
+                    message = self._format_message(
+                        {
+                            "self": self.service_id,
+                            "sensor": self.sensor_name,
+                            "sender": package.sender,
+                            "message": message,
+                            "response": response.result,
+                            "time": response.time,
+                            "memory": response.memory,
+                        }
+                    )
+                    logger.info(f"Sending data to channel {channel}: {message}")
+                    self.send(channel, message)
 
         except Exception as error:
-            logger.debug(f"Got an error performing action: {error}")
-            self.handle_error(error, package)
-            raise error
+            logger.info(f"Sending error data to channel error:{self.channel}: {error}")
+            self.send(
+                f"error:{self.channel}",
+                {
+                    "self": self.service_id,
+                    "sensor": self.sensor_name,
+                    "sender": package.sender,
+                    "message": package.message,
+                    "error": str(error),
+                    "traceback": "\n".join([item.strip() for item in format_traceback()]),
+                    "info": "\n".join([item.strip() for item in format_exception_info()]),
+                },
+            )
+        finally:
+            self.set_state(state_key, self.refine_state(state))
+            logger.debug("Cell cycle complete")
 
-        channel = package.sender if self.channel == "sender" else self.channel
-        communication_data = {
-            "sensor": self.sensor,
-            "sender": package.sender,
-            "message": message,
-            "response": response.result,
-            "time": response.time,
-            "memory": response.memory,
-        }
-
-        logger.info(f"Sending data to channel {channel}: {communication_data}")
-        self.send(channel, self.format_message(communication_data))
-
-    def _process_message(self, message, state):
-        model = self.get_provider("language_model", self.model_provider, **self.model_options)
+    def process_sensory_message(self, message, state):
+        model = self.get_provider("language_model", self.agent_model_provider, **self.agent_model_options)
         prompts = self._render_prompts(
             {
                 **state,
                 "message": message,
-                "field_labels": self.message_field_labels,
+                "field_labels": self.agent_message_field_labels,
                 "tools": self.mcp.list_tools(state["tools"]),
             }
         )
@@ -110,57 +152,57 @@ class Cell(BaseCommand("cell")):
 
         # raise CellError("Maximum amount of retries")
 
-    def refine_state(self, package, message, state):
+    def refine_state(self, state):
         return state
 
-    def get_sensor_key(self):
-        return self.sensor_key if self.sensor_key else self.channel
+    def _set_sensor(self, channel):
+        if channel not in self.channels:
+            self.error(f"Channel {channel} does not exist ot you do not have access")
 
-    def load_message(self, message):
-        self._sensor_data_type = None
+        self.sensor_name = channel
+        self.sensor_spec = self.manager.get_channel_spec(self.sensor_name)
+        if not self.sensor_spec:
+            self.error(f"Channel name {self.sensor_name} is not a valid sensory channel")
 
-        if self.sensor.startswith("data:save:"):
-            self._sensor_data_type = self.sensor.split(":")[2]
-        elif self.sensor_data:
-            self._sensor_data_type = self.sensor_data
+    def _load_message(self, package):
+        message = package.message
+        data_type = None
 
-        if self._sensor_data_type:
-            facade = self.facade(self._sensor_data_type)
+        sensor_spec_name = self.sensor_spec.name.split(":")
+        data_type_index = next((index for index, item in enumerate(sensor_spec_name) if item == "{data_type}"), None)
+
+        if data_type_index is not None:
+            data_type = self.sensor_name.split(":")[data_type_index]
+        elif self.agent_sensor_data:
+            data_type = self.agent_sensor_data
+
+        if data_type:
+            facade = self.facade(data_type)
             if isinstance(message, (str, int)):
-                instances = facade.values(*self.message_fields, id=message, **self.sensor_filters)
-                return instances[0] if len(instances) > 0 else None
+                instances = facade.values(*self.agent_message_fields, id=message, **self.agent_sensor_filters)
+                message = instances[0] if len(instances) > 0 else None
             else:
-                instances = facade.values(*self.message_fields, id=message[self.sensor_data_id], **self.sensor_filters)
-                return instances[0] if len(instances) > 0 else None
+                instances = facade.values(
+                    *self.agent_message_fields, id=message[self.agent_sensor_data_id], **self.agent_sensor_filters
+                )
+                message = instances[0] if len(instances) > 0 else None
 
         elif isinstance(message, dict):
-            return {name: value for name, value in message.items() if not self.message_fields or name in self.message_fields}
+            message = validate_flattened_dict(
+                flatten_dict(normalize_value(message), self.agent_message_fields),
+                self.agent_sensor_filters,
+            )
+        else:
+            if self.agent_message_fields and "message" not in self.agent_message_fields:
+                self.error("Message field message of scalar channel value it not specified")
+
+            message = validate_flattened_dict({"message": normalize_value(message)}, self.agent_sensor_filters)
 
         return message
 
-    def format_message(self, message):
+    def _format_message(self, message):
         # Override in subclasses if needed
         return message
-
-    def _check_message(self, message):
-        if not message:
-            return False
-
-        if not self._sensor_data_type:
-            for field, value in self.sensor_filters.items():
-                value = normalize_value(value)
-                # Filter field doesn't exist
-                if field not in message:
-                    return False
-                # Scalar message and filter values do not match
-                elif normalize_value(message[field]) != value:
-                    return False
-
-        return self.validate_message(message)
-
-    def validate_message(self, message):
-        # Override in subclasses if needed
-        return True
 
     def _render_prompts(self, state):
         prompts = copy.deepcopy(self.get_prompts())
@@ -172,18 +214,20 @@ class Cell(BaseCommand("cell")):
     def get_prompts(self):
         prompt_base = "cell/prompt"
         return {
-            "system": f"{prompt_base}/{self.system_template}.md",
-            "tools": f"{prompt_base}/{self.tools_template}.md",
-            "request": f"{prompt_base}/{self.template}.md",
+            "system": f"{prompt_base}/{self.agent_system_template}.md",
+            "tools": f"{prompt_base}/{self.agent_tools_template}.md",
+            "request": f"{prompt_base}/{self.agent_template}.md",
         }
 
     def get_memory_index(self, message):
         try:
-            if self.chat_key == "global":
-                return self.sensor_key
-            return f"{self.sensor_key}:{message[self.chat_key]}"
+            if self.agent_chat_key == "global":
+                return self.agent_sensor_key
+            return f"{self.agent_sensor_key}:{message[self.agent_chat_key]}"
         except KeyError:
-            raise CellError(f"Memory index chat key {self.chat_key} is not global and does not exist in message: {message}")
+            raise CellError(
+                f"Memory index chat key {self.agent_chat_key} is not global and does not exist in message: {message}"
+            )
 
     def get_chat(self, message):
         if not getattr(self, "_chat_index", None):
@@ -229,4 +273,4 @@ class Cell(BaseCommand("cell")):
 
     def handle_error(self, error, package):
         # Repeat if failure so we don't lose the message
-        self.send(self.sensor, package.message, package.sender)
+        self.send(self.sensor_name, package.message, package.sender)
