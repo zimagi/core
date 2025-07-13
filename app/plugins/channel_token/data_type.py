02a8c523d0edcefc778c8dcf2c09ad48b4f050c7
Adrian Webb <adrian@webb.sh>
2025-07-13 18:10:42 -0400

    Adding an initial channel token plugin and data type provider.

---
+++ app/plugins/channel_token/data_type.py
@@ -0,0 +1,21 @@
+from systems.plugins.index import BaseProvider
+from .base import ChannelTokenParseError
+
+
+class Provider(BaseProvider("channel_token", "data_type")):
+
+    def __init__(self, type, name, command, **config):
+        super().__init__(type, name, command, **config)
+        if self.field_value not in self.command.manager.index.get_facade_index():
+            raise ChannelTokenParseError(f"Data type {self.field_value} does not exist")
+
+        self.facade = self.command.facade(self.field_value, False)
+
+    def load(self, message):
+        if isinstance(message, (str, int)):
+            instances = self.facade.values(*self.field_fields, id=message, **self.field_filters)
+            message = instances[0] if len(instances) > 0 else None
+        else:
+            instances = self.facade.values(*self.field_fields, id=message[self.field_id_field], **self.field_filters)
+            message = instances[0] if len(instances) > 0 else None
+        return message
