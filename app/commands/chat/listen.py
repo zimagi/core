e356233c3f075abf2b6b38373825f02c054ffb7e
Adrian Webb <adrian@webb.sh>
2025-07-18 18:45:45 -0400

    Updates to the chat send and listen command implementations.

--- app/commands/chat/listen.py
+++ app/commands/chat/listen.py
@@ -1,3 +1,5 @@
+import math
+
 from systems.commands.index import Command
 
 
@@ -8,13 +10,14 @@ class Listen(Command("chat.listen")):
             "chat:message",
             state_key=self.listen_state_key if self.listen_state_key else f"core_chat_{self.active_user.name}",
             timeout=self.listen_timeout,
-            block_sec=self.listen_timeout,
+            block_sec=math.ceil(self.listen_timeout / 2),
         ):
             chat_names = self._chat.field_values("name", user=self.active_user)
             message = package.message
 
-            if message["name"] in chat_names and message["user"] != self.active_user.name:
+            if message["name"] in chat_names:
                 self.data("Message", message, "message")
-                self.save_user_message(
-                    message["name"], message["message"], time=message["time"], user=message["user"], role="assistant"
-                )
+                if message["user"] != self.active_user.name:
+                    self.save_user_message(
+                        message["name"], message["message"], time=message["time"], user=message["user"], role="assistant"
+                    )
--- app/commands/chat/send.py
+++ app/commands/chat/send.py
@@ -4,15 +4,15 @@ from systems.commands.index import Command
 class Send(Command("chat.send")):
 
     def exec(self):
-        if self.chat_message:
+        if self.chat_text:
             time = self.time.now_string
             self.send(
                 "chat:message",
                 {
                     "user": self.active_user.name,
                     "name": self.chat_name,
-                    "message": self.chat_message,
+                    "message": self.chat_text,
                     "time": time,
                 },
             )
-            self.save_user_message(self.chat_name, self.chat_message, time=time)
+            self.save_user_message(self.chat_name, self.chat_text, time=time)
