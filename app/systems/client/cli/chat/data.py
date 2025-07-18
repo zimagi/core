ec605414dbbfb8d9f2989b92fc28b943c5670bdf
Adrian Webb <adrian@webb.sh>
2025-07-18 18:47:39 -0400

    Updating the chat message data constructor parameters in the system CLI client chat data library.

--- app/systems/client/cli/chat/data.py
+++ app/systems/client/cli/chat/data.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 
 class ChatMessage:
-    def __init__(self, content, time=None, sender="system", is_user=False):
+
+    def __init__(self, content, sender, time=None, is_user=False):
         self.content = content
         self.sender = sender
         self.is_user = is_user
