336b277aba6f2d7087d36f39caf361a58484aae5
Adrian Webb <adrian@webb.sh>
2025-07-19 14:36:40 -0400

    Fixing the missing DeepInfra API key and updating the field dimension name in the encoder providers.

--- app/plugins/encoder/deepinfra.py
+++ app/plugins/encoder/deepinfra.py
@@ -1,7 +1,7 @@
+import os
 import time
 
 import requests
-from django.conf import settings
 from systems.plugins.index import BaseProvider
 from utility.data import ensure_list, load_json
 
@@ -16,18 +16,23 @@ class Provider(BaseProvider("encoder", "deepinfra")):
         if not text:
             return []
 
+        deepinfra_api_key = os.environ.get("DEEPINFRA_API_KEY", None)
+        if not deepinfra_api_key:
+            self.command.error(
+                "To use the DeepInfra encoder provider you must specify a DEEPINFRA_API_KEY environment variable"
+            )
         for retry in range(3):
             try:
                 response = requests.post(
                     "https://api.deepinfra.com/v1/openai/embeddings",
                     headers={
-                        "Authorization": f"Bearer {settings.DEEPINFRA_API_KEY}",
+                        "Authorization": f"Bearer {deepinfra_api_key}",
                         "Content-Type": "application/json",
                     },
                     timeout=2000,
                     json={
                         "input": ensure_list(text),
-                        "dimensions": self.field_dimensions,
+                        "dimensions": self.field_dimension,
                         "encoding_format": self.field_format,
                         "model": self.field_model,
                     },
