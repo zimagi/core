a4cb2b3234266850e17d35f2beed5351be9a7b9c
Adrian Webb <adrian@webb.sh>
2025-08-01 05:54:00 -0400

    Updating test and package configurations for the Javascript SDK.

--- sdk/javascript/jest.config.cjs
+++ sdk/javascript/jest.config.cjs
@@ -4,4 +4,8 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
 };
