6084a0885101d8754a827ae9efd844fdf9645392
Adrian Webb <adrian@webb.sh>
2025-08-19 12:55:55 -0400

    Refactoring the text parsing in the parse_webpage method of the browser command mixin.

--- app/commands/mixins/browser.py
+++ app/commands/mixins/browser.py
@@ -1,6 +1,5 @@
-import re
-
 from bs4 import BeautifulSoup
+from html_to_markdown import convert_to_markdown
 from systems.commands.index import CommandMixin
 from utility.data import Collection
 from utility.web import WebParser
@@ -11,25 +10,19 @@ class BrowserMixin(CommandMixin("browser")):
     def parse_webpage(self, url):
         webpage = self.submit("browser:request", url, timeout=30)
         browser = None
-        text = ""
+        source = ""
 
         if webpage["source"]:
             source = webpage["source"]
-            soup, text = self._parse_webpage_text(webpage["source"])
-        if not text:
+        if not source:
             browser = WebParser(url, verify=False)
             source = browser.text
-            soup, text = self._parse_webpage_text(browser.text)
 
+        soup = BeautifulSoup(source, "lxml")
         return Collection(
             url=webpage["url"] if not browser else url,
             title=soup.title.text.encode("ascii", errors="ignore").decode().strip() if soup.title else "",
             source=source,
-            text=text,
+            text=convert_to_markdown(soup),
             soup=soup,
         )
-
-    def _parse_webpage_text(self, text):
-        soup = BeautifulSoup(text, "html.parser")
-        text = re.sub(r"\n+", "\n", soup.get_text("\n")).encode("ascii", errors="ignore").decode().strip()
-        return soup, text
