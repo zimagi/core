e65217496c233ea6a27d0bd017cb9d041691ba93
Adrian Webb <adrian@webb.sh>
2025-09-06 07:09:32 -0400

    Updating the PDF file parser plugin provider to use Docling DocTR OCR implementation instead of RapidOCR.

--- app/plugins/file_parser/pdf.py
+++ app/plugins/file_parser/pdf.py
@@ -1,13 +1,14 @@
 import os
 
 from django.conf import settings
-from docling.datamodel.pipeline_options import PdfPipelineOptions, RapidOcrOptions
+from docling.datamodel.pipeline_options import PdfPipelineOptions
 from docling.document_converter import (
     ConversionResult,
     DocumentConverter,
     InputFormat,
     PdfFormatOption,
 )
+from docling_ocr_onnxtr import OnnxtrOcrOptions
 
 from systems.plugins.index import BaseProvider
 
@@ -15,26 +16,17 @@ from systems.plugins.index import BaseProvider
 class Provider(BaseProvider("file_parser", "pdf")):
 
     def parse_file(self, file_path):
-        os.environ["HF_HOME"] = settings.MANAGER.hf_cache
-
-        from huggingface_hub import snapshot_download
-
-        download_path = snapshot_download(repo_id="SWHL/RapidOCR")
-        det_model_path = os.path.join(download_path, "PP-OCRv4", "en_PP-OCRv3_det_infer.onnx")
-        rec_model_path = os.path.join(download_path, "PP-OCRv4", "ch_PP-OCRv4_rec_server_infer.onnx")
-        cls_model_path = os.path.join(download_path, "PP-OCRv3", "ch_ppocr_mobile_v2.0_cls_train.onnx")
-        ocr_options = RapidOcrOptions(
-            det_model_path=det_model_path,
-            rec_model_path=rec_model_path,
-            cls_model_path=cls_model_path,
+        ocr_options = OnnxtrOcrOptions(
+            det_arch="db_mobilenet_v3_large",
+            reco_arch="Felix92/onnxtr-parseq-multilingual-v1",
         )
-
         pipeline_options = PdfPipelineOptions(
-            do_ocr=True,
-            do_table_structure=True,
-            table_structure_options={"do_cell_matching": True},
             ocr_options=ocr_options,
         )
+        pipeline_options.allow_external_plugins = True
+        pipeline_options.do_table_structure = True
+        pipeline_options.table_structure_options.do_cell_matching = True
+
         converter = DocumentConverter(
             format_options={
                 InputFormat.PDF: PdfFormatOption(
